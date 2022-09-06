a0bfd8108dd4d44f254d3c5a614e5fa3abc222cb
Oncu <oohancan@gmail.com>
2022-09-06 11:47:21 +0300

    add resources and VideoListView

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Oncu Can on 6.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
