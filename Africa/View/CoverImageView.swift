5fc43ddd04bdd66545f22054b1540773430fba5f
Oncu <oohancan@gmail.com>
2022-09-09 20:11:48 +0300

    add CoverImageView

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Oncu Can on 9.09.2022.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+    }
+}
