dbc9787a22b09a4607fc69325de5d0b310d74235
Oncu <oohancan@gmail.com>
2022-09-10 09:09:17 +0300

    add CoverImageModel

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Oncu Can on 10.09.2022.
+//
+
+import Foundation
--- Africa/View/CoverImageView.swift
+++ Africa/View/CoverImageView.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct CoverImageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
 struct CoverImageView_Previews: PreviewProvider {
     static var previews: some View {
         CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
     }
 }
