942c6c6f4339cbc911f4a1b9831176b1c7bcc01f
Oncu <oohancan@gmail.com>
2022-09-11 16:32:11 +0300

    add struct Animal

--- Africa/View/AnimalListItemView.swift
+++ Africa/View/AnimalListItemView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct AnimalListItemView: View {
+    let animal: Animal
     var body: some View {
         HStack(alignment: .center, spacing: 16) {
-            Image("lion")
+            Image(animal.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 90, height: 90)
@@ -18,12 +19,12 @@ struct AnimalListItemView: View {
                 RoundedRectangle(cornerRadius: 12))
             
             VStack(alignment: .leading, spacing: 8) {
-                Text("Lion")
+                Text(animal.name)
                     .font(.title2)
                     .fontWeight(.heavy)
                     .foregroundColor(.accentColor)
                 
-                Text("The World's most social felines, lions roam the savannas and grasslands of the African Continent, hunting cooperatively and raising cubs in prides.")
+                Text(animal.headline)
                     .font(.footnote)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
@@ -35,8 +36,10 @@ struct AnimalListItemView: View {
 }
 
 struct AnimalListItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals-json")
+    
     static var previews: some View {
-        AnimalListItemView()
+        AnimalListItemView(animal: animals[1])
             .previewLayout(.sizeThatFits)
             .padding()
     }
