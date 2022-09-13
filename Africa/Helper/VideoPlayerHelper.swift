5e1c27110ef1ed83fd511ef64a63bc8c2f7e657f
Oncu <oohancan@gmail.com>
2022-09-13 11:39:46 +0300

    add VideoPlayerView, videoPlayer model and NavigationLink for VideoListView

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Oncu Can on 13.09.2022.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
